dd555997a5fad756dccf7cfd86e20acc3d29d95d
zhang <1498051123@qq.com>
2020-02-08 10:18:24 +0800

    把本地工程添加到git

---
+++ git-test-project/src/edu/uestc/FirstClass.java
@@ -0,0 +1,11 @@
+package edu.uestc;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hellogit");
+        System.out.println("Hellogit");
+        System.out.println("Hellogit");
+        System.out.println("Hellogit");
+        System.out.println("Hellogit");
+    }
+}
